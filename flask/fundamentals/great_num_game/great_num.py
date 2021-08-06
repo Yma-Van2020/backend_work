267f2c23c646f6e1775802ab00ab4b059027bce4
Yucong Ma <candicema2018@gmail.com>
2021-08-06 10:39:56 -0700

    finished

--- flask/fundamentals/great_num_game/great_num.py
+++ flask/fundamentals/great_num_game/great_num.py
@@ -6,21 +6,17 @@ app.secret_key = 'banana'
 
 @app.route("/")
 def page():
-   random_num1 = random.randint(1, 100) 
-   random_num2 = random.randint(1, 100) 
-   random_num3 = random.randint(1, 100) 
-   random_num4 = random.randint(1, 100) 
-   random_num5 = random.randint(1, 100) 
-   random_num6 = random.randint(1, 100) 
+       
+   context = {
+   "random_num1" : random.randint(1, 100), 
+   "random_num2" : random.randint(1, 100), 
+   "random_num3" : random.randint(1, 100), 
+   "random_num4" : random.randint(1, 100), 
+   "random_num5" : random.randint(1, 100),
+   "random_num6" : random.randint(1, 100),
+   }
    
-   style = ""
-   if 'visits' in session:
-        session['visits'] = session.get('visits') + 1  # reading and updating session data
-   else:
-        session['visits'] = 1 # setting session data
-   return render_template("index.html", visit = session['visits'], style = style, 
-                          random_num1 = random_num1,random_num2 = random_num2,random_num3 = random_num3
-                          ,random_num4 = random_num4,random_num5 = random_num5,random_num6= random_num6)
+   return render_template("index.html", **context)
 
 @app.route("/clear_session")
 def clear_visit():
@@ -29,7 +25,7 @@ def clear_visit():
     
 @app.route("/guess", methods=['POST'])
 def guess_random():
-   session['num'] = int(request.form['num_dis'])
+   session['num'] = int(request.form["comp_select"])
    
    return redirect("/show")	 
 
@@ -38,17 +34,17 @@ def show_guess():
    random_num = random.randint(1, 100) 
    	
    if random_num == session['num']:
-      reply = 'You got it right, wanna play again?'
-      style = "green_style"
+      session['reply'] = 'You got it right, wanna play again?'
+      session['style'] = "green_style"
    
    elif random_num > session['num']: 
-      reply = 'Too low!'
-      style = "reg_style"
+      session['reply'] = 'Too low!'
+      session['style'] = "reg_style"
    elif random_num < session['num']:
-      reply = 'Too high!'  
-      style = "reg_style"
+      session['reply'] = 'Too high!'  
+      session['style'] = "reg_style"
  
-   return render_template("index.html", reply = reply, style = style)
+   return redirect("/")
        
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)  
